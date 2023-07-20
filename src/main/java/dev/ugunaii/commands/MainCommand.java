c328a3f2e3dc1570735d20a961699a1368be6956
GodGotzi <eli.gottsbacher@gmail.com>
2023-07-20 20:26:44 +0200

    Better readability, added some not null statements

--- src/main/java/dev/ugunaii/commands/MainCommand.java
+++ src/main/java/dev/ugunaii/commands/MainCommand.java
@@ -4,60 +4,60 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import dev.ugunaii.Main;
+import dev.ugunaii.FeelingAnimals;
 import dev.ugunaii.utils.Message;
+import org.jetbrains.annotations.NotNull;
 
 public class MainCommand implements CommandExecutor {
 
   // Acces the main class.
-  private Main plugin;
+  private final FeelingAnimals plugin;
 
-  public MainCommand(Main plugin) {
+  public MainCommand(FeelingAnimals plugin) {
     this.plugin = plugin;
   }
 
   @Override
-  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+  public boolean onCommand(CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
     // As all of the commands can be executed by anyone, there is no need to check
     // if the sender is a player or not.
 
     // Check if the command sender has permissions.
-    if (sender.hasPermission("feelinganimals.commands")) {
-
-      // Check if ther are any args.
-      if (args.length > 0) {
-
-        if (args[0].equalsIgnoreCase("reload")) { // Command to reload the config file.
-          plugin.reloadConfig();
-          Message.sendCommand(sender, getMessage("config-reloaded"));
-
-        } else if (args[0].equalsIgnoreCase("version")) { // Command to display the plugin version.
-          Message
-              .sendCommand(sender, getMessage("display-version"));
-
-        } else if (args[0].equalsIgnoreCase("help")) { // Command to display the help manu.
-          Message.sendCommand(sender, getMessage("display-help"));
-
-        } else { // If the argument is not valid.
-          Message.sendCommand(sender, getMessage("invalid-argument"));
-          Message.sendCommand(sender, getMessage("get-help"));
-        }
-
-        // If there aren't any args.
-      } else {
-        Message.sendCommand(sender, getMessage("argument-needed"));
-        Message.sendCommand(sender, getMessage("get-help"));
-      }
-      // If the player doesn't have the permission.
-    } else {
+    if (!sender.hasPermission("feelinganimals.commands")) {
       Message.sendCommand(sender, getMessage("no-permission"));
+
+      return true;
+    }
+
+    // Check if ther are any args.
+    if (args.length == 0) {
+      Message.sendCommand(sender, getMessage("argument-needed"));
+      Message.sendCommand(sender, getMessage("get-help"));
+
+      return true;
+    }
+
+    if (args[0].equalsIgnoreCase("reload")) { // Command to reload the config file.
+      plugin.reloadConfig();
+      Message.sendCommand(sender, getMessage("config-reloaded"));
+
+    } else if (args[0].equalsIgnoreCase("version")) { // Command to display the plugin version.
+      Message
+          .sendCommand(sender, getMessage("display-version"));
+
+    } else if (args[0].equalsIgnoreCase("help")) { // Command to display the help manu.
+      Message.sendCommand(sender, getMessage("display-help"));
+
+    } else { // If the argument is not valid.
+      Message.sendCommand(sender, getMessage("invalid-argument"));
+      Message.sendCommand(sender, getMessage("get-help"));
     }
 
     return true;
   }
 
-  // Method to get messages from th econfig.
+  // Method to get messages from the config.
   private String getMessage(String path) {
     return plugin.getConfig().getString("Messages." + path);
   }
--- src/main/java/dev/ugunaii/events/PunchAnimal.java
+++ src/main/java/dev/ugunaii/events/PunchAnimal.java
@@ -3,6 +3,7 @@ package dev.ugunaii.events;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Husk;
@@ -12,14 +13,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
-import dev.ugunaii.Main;
+import dev.ugunaii.FeelingAnimals;
 
 public class PunchAnimal implements Listener {
 
   // Acces the main class.
-  private Main plugin;
+  private final FeelingAnimals plugin;
 
-  public PunchAnimal(Main plugin) {
+  public PunchAnimal(FeelingAnimals plugin) {
     this.plugin = plugin;
   }
 
@@ -28,43 +29,52 @@ public class PunchAnimal implements Listener {
   public void onEntityDamage(EntityDamageByEntityEvent event) {
 
     // Check if the module is enabled in the config.
-    if (plugin.getConfig().getString("Config.enabled").equalsIgnoreCase("true")) {
+    String enabled = plugin.getConfig().getString("Config.enabled");
+    if (enabled == null || !enabled.equalsIgnoreCase("true")) {
+      return;
+    }
 
-      // Check if the entity that is punching is a player.
-      if (event.getDamager() instanceof Player) {
+    // Check if the entity that is punching is a player.
+    if (!(event.getDamager() instanceof Player)) {
+      return;
+    }
 
-        /// Check if the punched animal is in the config list.
-        if (plugin.getConfig().getStringList("Config.animal-list").contains(event.getEntity().getType().name())) {
 
-          // Create variables.
-          Player damager = (Player) event.getDamager();
+    // Check if the punched animal is in the config list.
+    if (plugin.getConfig().getStringList("Config.animal-list")
+            .contains(event.getEntity().getType().name())) {
 
-          // Get a list of the animals in the given radius.
-          double detectionRadius = plugin.getConfig().getDouble("Config.distance-radius");
-          List<Entity> nearbyEntities = (List<Entity>) damager.getWorld().getNearbyEntities(damager.getLocation(),
+      // Create variables.
+      Player damager = (Player) event.getDamager();
+
+      // Get a list of the animals in the given radius.
+      double detectionRadius = plugin.getConfig().getDouble("Config.distance-radius");
+
+      List<Entity> nearbyEntities = (List<Entity>) damager.getWorld().getNearbyEntities(damager.getLocation(),
               detectionRadius, detectionRadius, detectionRadius);
 
-          // Remove the punched animal from the list.
-          nearbyEntities.remove(event.getEntity());
+      // Remove the punched animal from the list.
+      nearbyEntities.remove(event.getEntity());
 
-          // Also remove the player from the list.
-          nearbyEntities.remove(damager);
+      // Also remove the player from the list.
+      nearbyEntities.remove(damager);
 
-          // For every entity in the radius, call the flee method.
-          for (Entity e : nearbyEntities) {
-            makeEntityFlee(((LivingEntity) e));
-          }
-        }
+      // For every entity in the radius, call the flee method.
+      for (Entity e : nearbyEntities) {
+        makeEntityFlee(((LivingEntity) e));
       }
+
     }
 
   }
 
   // Method to make the animal flee.
   private void makeEntityFlee(LivingEntity e) {
+    World world = e.getLocation().getWorld();
+    assert world != null;
 
     // Create a fake entity.
-    Husk fakeEntity = (Husk) e.getLocation().getWorld().spawnEntity(e.getLocation(), EntityType.HUSK);
+    Husk fakeEntity = (Husk) world.spawnEntity(e.getLocation(), EntityType.HUSK);
 
     // Give the fake entity params.
     fakeEntity.setInvisible(true);
@@ -77,7 +87,7 @@ public class PunchAnimal implements Listener {
     double initialhealth = e.getHealth();
 
     // Make the fake entity punch the animal to scare him.
-    ((LivingEntity) fakeEntity).attack(e);
+    fakeEntity.attack(e);
 
     // Destore the initial health of the animal so it stays the same.
     e.setHealth(initialhealth);
